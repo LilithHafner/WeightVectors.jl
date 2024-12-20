393485209a37df7b2f89fa69b17afa6e25e247c4
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-20 23:10:32 +0100

    Add statistical tests (#7)
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using DynamicDiscreteSamplers
+using HypothesisTests
 using Random
 using Test
 using Aqua
+using Random
+using StatsBase
 
 @testset "unit tests" begin
     lls = DynamicDiscreteSamplers.LinkedListSet3()
@@ -87,6 +90,77 @@ end
     end
     delete!(ds, 2)
     @test count(rand(ds) == 1 for _ in 1:4000) < 1200 # False positivity rate < 4e-13
+end
+  
+@testset "Randomized statistical tests" begin
+    Random.seed!(42)
+    b = 100
+    range = 1:b
+    weights = [Float64(i) for i in range]
+
+    ds1 = DynamicDiscreteSampler()
+    for (i, w) in zip(range, weights)
+        push!(ds1, i, w)
+    end
+
+    samples_counts = countmap([rand(ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in 1:b]
+    ps_exact = [i/((b ÷ 2)*(b+1)) for i in 1:b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.00005
+
+    samples_counts = countmap([rand(ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in 1:b]
+    ps_exact = [i/((b ÷ 2)*(b+1)) for i in 1:b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.00005
+
+    for i in 1:(b ÷ 2)
+        delete!(ds1, i)
+    end
+
+    samples_counts = countmap([rand(ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in (b ÷ 2 + 1):b]
+    ps_exact = [i/((b ÷ 2)*(b+1) - (b ÷ 4)*(b ÷ 2 + 1)) for i in (b ÷ 2 + 1):b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.00005
+
+    ds2 = DynamicDiscreteSampler()
+    
+    for (i, w) in zip(range, weights)
+        push!(ds2, i, w)
+    end
+
+    delete!(ds2, 1)
+    delete!(ds2, 2)
+
+    push!(ds2, 2, 200.0)
+    push!(ds2, 1000, 1000.0)
+
+    samples_counts = countmap([rand(ds2) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in [2:b..., 1000]]
+    wsum = (b ÷ 2)*(b+1) - 3 + 200 + 1000
+    ps_exact = [i == 2 ? 200/wsum : i/wsum for i in [2:b..., 1000]]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.00005
+
+    for i in [2:b..., 1000]
+        delete!(ds2, i)
+    end
+
+    push!(ds2, 1, 1.0)
+    push!(ds2, 2, 2.0)
+    samples_counts = countmap([rand(ds2) for _ in 1:10^4])
+    counts_est = [samples_counts[1], samples_counts[2]]
+    ps_exact = [1/3, 2/3]
+    @test pvalue(chisq_test) > 0.00005
+
+    delete!(ds2, 2)
+    @test unique([rand(ds2) for _ in 1:10^3]) == [1]
 end
 
 # These tests are too slow:
