0f06072c63987be708dadf61861b58e4bc1de814
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-12-23 14:18:57 +0100

    Update DynamicDiscreteSamplers.jl

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -358,6 +358,7 @@ function Base.rand(rng::AbstractRNG, ns::NestedSampler5, n::Integer)
 end
 Base.rand(ns::NestedSampler5) = rand(Random.default_rng(), ns)
 function Base.rand(rng::AbstractRNG, ns::NestedSampler5)
+    ns.reset_order[] += 1
     lastfull = length(ns.sampled_levels)
     ns.reset_distribution[] && set_weights!(ns.distribution_over_levels, ns)
     ns.reset_distribution[] = false
