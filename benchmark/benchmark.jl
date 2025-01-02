f134ad2001ded35874e4ca18223e8c8f66cd893d
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-01-02 21:13:04 +0100

    Improve benchmark

--- benchmark/benchmark.jl
+++ benchmark/benchmark.jl
@@ -1,10 +1,9 @@
-
-using DynamicDiscreteSamplers, Chairmarks, Random
+using DynamicDiscreteSamplers, Chairmarks, Random, Statistics
 
 function setup(rng, indices)
     ds = DynamicDiscreteSampler()
     for i in indices
-	push!(ds, i, (10^200)*rand(rng))
+        push!(ds, i, (10.0^200)*rand(rng))
     end
     return ds
 end
@@ -18,19 +17,19 @@ end
 # Sampling with Variable Distribution
 
 function sample_variable(rng, ds, n)
-   inds = Vector{Int}(undef, n)
-   for i in 1:n
-	j = rand(rng, 1:n)
-	delete!(ds, j)
-	inds[i] = rand(rng, ds)
-	push!(ds, j, (10^200)*rand(rng))
-   end
-   return ds
+    inds = Vector{Int}(undef, n)
+    @inbounds for i in 1:n
+        j = rand(rng, 1:n)
+        delete!(ds, j)
+        inds[i] = rand(rng, ds)
+        push!(ds, j, (10.0^200)*rand(rng))
+    end
+    return ds
 end
 
 rng = Xoshiro(42)
-for s in [10^i for i in 3:8]
-    b1 = @b setup($rng, 1:$s) sample_fixed($rng, _, $s)
-    b2 = @b setup($rng, 1:$s) sample_variable($rng, _, $s)
-    println(b1.time/s*10^9, " ", b2.time/s*10^9,)
+for s in [10^i for i in 1:8]
+    b1 = @be setup($rng, 1:$s) sample_fixed($rng, _, $s) seconds=3
+    b2 = @be setup($rng, 1:$s) sample_variable($rng, _, $s) seconds=3
+    println(mean(x.time for x in b1.samples)/s*10^9, " ", mean(x.time for x in b2.samples)/s*10^9,)
 end
