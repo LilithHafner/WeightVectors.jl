234c9c0ba97ff66ff35936701970bf23e48a40dd
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-01-10 16:24:56 +0100

    Update update_levels_new_vs_present.jl

--- benchmark/update_levels_new_vs_present.jl
+++ benchmark/update_levels_new_vs_present.jl
@@ -2,7 +2,7 @@ using Plots, Chairmarks, DynamicDiscreteSamplers
 function push_v!(ds, n)
     k = ds.track_info.nvalues
     for i in k+1:k+n
-        push!(ds, i, 2.0^i)
+        push!(ds, i, (2.0^i)+eps())
     end
     return ds
 end
@@ -21,9 +21,9 @@ y2 = [(@b push_v!(DynamicDiscreteSampler(), xi) push_v!(_, xi) evals=1 seconds=.
 y3 = [(@b push_v!(DynamicDiscreteSampler(), xi) remove_v!(_, xi) evals=1 seconds=.01).time for xi in x];
 y4 = [(@b push_v!(push_v!(DynamicDiscreteSampler(), xi), xi) remove_v!(_, xi) evals=1 seconds=.01).time for xi in x];
 
-plot(x,y2,label="add element");
-plot(x,y1,label="add element + level");
-plot!(x,y4,label="remove element");
-plot!(x,y3,label="remove element + level")
+plot(x,y1,label="push! - new sampled levels");
+plot!(x,y2,label="push! - present sampled levels");
+plot!(x,y3,label="delete! - new sampled levels");
+plot!(x,y4,label="delete! - present sampled levels")
 
 savefig("update_levels_new_vs_present.png")
