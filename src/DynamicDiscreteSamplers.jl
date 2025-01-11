100cfe4d5df307e343f08133266f21ae585d3a59
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-01-11 18:50:39 +0100

    fix prob

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -492,8 +492,8 @@ function Base.rand(rng::AbstractRNG, fs::FallBackSampler, ns::NestedSampler, las
         level = exponent(level_sampler.data[1][2])
         totwnots += flot(wlevel, level)
     end
-    totw = totwnots + ns.distribution_over_levels.p[lastfull]    
-    r = (typemax(UInt)/UPPER_LIMIT) * (totwnots/totw)
+    totw = totwnots + ns.distribution_over_levels.p[lastfull]
+    r = (totwnots/totw) / (typemax(UInt64)/UPPER_LIMIT)
     rand(rng) > r && return 0
     u = rand(rng)*totwnots
     last = 0
