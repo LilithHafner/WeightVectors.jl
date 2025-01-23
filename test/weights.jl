393de112f54dda6eac362e15ece4091cea764b0c
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-01-23 15:07:12 -0600

    Make constructable

--- test/weights.jl
+++ test/weights.jl
@@ -1,1 +1,5 @@
-using DynamicDiscreteSamplers
+using DynamicDiscreteSamplers, Test
+
+@test DynamicDiscreteSamplers.FixedSizeWeights(10) isa DynamicDiscreteSamplers.FixedSizeWeights
+@test DynamicDiscreteSamplers.ResizableWeights(10) isa DynamicDiscreteSamplers.ResizableWeights
+@test DynamicDiscreteSamplers.SemiResizableWeights(10) isa DynamicDiscreteSamplers.SemiResizableWeights
