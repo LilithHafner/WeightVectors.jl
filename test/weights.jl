3b8c9de2237cba06b4a4cf26114a78cf61ed441f
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-01-23 15:11:50 -0600

    Add some tests

--- test/weights.jl
+++ test/weights.jl
@@ -3,3 +3,11 @@ using DynamicDiscreteSamplers, Test
 @test DynamicDiscreteSamplers.FixedSizeWeights(10) isa DynamicDiscreteSamplers.FixedSizeWeights
 @test DynamicDiscreteSamplers.ResizableWeights(10) isa DynamicDiscreteSamplers.ResizableWeights
 @test DynamicDiscreteSamplers.SemiResizableWeights(10) isa DynamicDiscreteSamplers.SemiResizableWeights
+
+w = DynamicDiscreteSamplers.FixedSizeWeights(10)
+
+@test_throws ArgumentError("collection must be non-empty") rand(w)
+
+@test 1 === (w[1] = 1)
+
+@test_broken rand(w) === 1
