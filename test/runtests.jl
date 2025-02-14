dbfcf1c56365dd4fbbc11fe196c4ec2c8769aff3
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-02-14 17:57:23 +0100

    readd test

--- test/runtests.jl
+++ test/runtests.jl
@@ -134,13 +134,12 @@ end
     ds2[1000] = 1000.0
 
     samples_counts = countmap(rand(rng, ds2, 10^5))
-    # TODO: this throws by not finding index 2 in the samples
-    #counts_est = [samples_counts[i] for i in [2:b..., 1000]]
-    #wsum = (b ÷ 2)*(b+1) - 3 + 200 + 1000
-    #ps_exact = [i == 2 ? 200/wsum : i/wsum for i in [2:b..., 1000]]
+    counts_est = [samples_counts[i] for i in [2:b..., 1000]]
+    wsum = (b ÷ 2)*(b+1) - 3 + 200 + 1000
+    ps_exact = [i == 2 ? 200/wsum : i/wsum for i in [2:b..., 1000]]
 
-    #chisq_test = ChisqTest(counts_est, ps_exact)
-    #@test pvalue(chisq_test) > 0.002
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
 
     for i in [2:b..., 1000]
         ds2[i] = 0.0
