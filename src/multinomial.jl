cd7a05ff03d28e5466c985cee35b745b733cb7cd
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-03-09 19:56:25 +0100

    Use exact multinomial for bulk rand

---
+++ src/multinomial.jl
@@ -0,0 +1,63 @@
+
+using AliasTables
+
+const tables = [
+	AliasTable{UInt128}(UInt128.([binomial(1,i) for i in 0:1])), 
+  AliasTable{UInt128}(UInt128.([binomial(2,i) for i in 0:2])),
+	AliasTable{UInt128}(UInt128.([binomial(4,i) for i in 0:4])), 
+	AliasTable{UInt128}(UInt128.([binomial(8,i) for i in 0:8])),
+	AliasTable{UInt128}(UInt128.([binomial(16,i) for i in 0:16])), 
+	AliasTable{UInt128}(UInt128.([binomial(32,i) for i in 0:32])),
+	AliasTable{UInt128}(UInt128.([binomial(64,i) for i in 0:64])), 
+  AliasTable{UInt128}(UInt128.([binomial(BigInt(128),i) for i in 0:128]))
+]
+
+function binomial_int(rng, trials, px, py)
+    if trials == 0 || px == 0
+		return 0
+    elseif px == py
+        return trials
+    end
+    count = 0
+    if px * 2 == py
+        count += binomial_int_12(rng, trials)
+    else
+        pw = Rational(px, py)
+        pt = Rational(1, 2)
+        while trials > 0
+            c = binomial_int_12(rng, trials)
+            if pw >= pt
+                count += c
+                trials -= c
+                pw -= pt
+            else
+                trials = c
+            end
+            pt /= 2
+        end
+    end
+    return count
+end
+
+function binomial_int_12(rng, trials)
+    count = 0
+    @inbounds while trials != 0
+        p = min(7, exponent(trials)) + 1
+        count += rand(rng, tables[p]) - 1
+        trials -= 1 << (p-1)
+    end
+    return count
+end
+
+function multinomial_int(rng, trials, weights)
+    cum_weights = cumsum(weights)
+    counts = Vector{Int}(undef, length(weights))
+    @inbounds for i in 1:length(weights)
+        n = weights[i]
+        d = cum_weights[end] - ((i>1) ? cum_weights[i-1] : 0)
+        b = binomial_int(rng, trials, n, d)
+        counts[i] = b
+        trials -= b
+    end
+    return counts
+end
