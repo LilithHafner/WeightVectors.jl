c92322ed347ec634b3ac2e571f8637f318f3da62
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-03-10 00:53:49 +0100

    Update DynamicDiscreteSamplers.jl

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -164,10 +164,12 @@ Base.rand(rng::AbstractRNG, w::Weights) = _rand(rng, w.m)
 Base.getindex(w::Weights, i::Int) = _getindex(w.m, i)
 Base.setindex!(w::Weights, v, i::Int) = (_setindex!(w.m, Float64(v), i); w)
 
+const bigpowers2 = [BigInt(2)^i for i in 1:2046]
+
 function _rand(rng::AbstractRNG, m::Memory{UInt64}, n::Integer)
     n < 10000 && return [_rand(rng, m) for _ in 1:n]
     i = _convert(Int, m[2])
-    weights = [get_significand_sum(m, j)*(BigInt(2)^j) for j in i:-1:5]
+    weights = [get_significand_sum(m, j)*bigpowers2[j-4] for j in i:-1:5]
     counts = multinomial_int(rng, n, weights)
     samples = Vector{Int}(undef, n)
     k = 1
