701a802365340a9e0dfdd949e261a12ca8229588
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-03-11 15:10:09 +0100

    Update benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -83,7 +83,7 @@ end
 using Random
 
 for n in [10^3, 10^6], k in [10^4, 10^5, 10^6], σ in [.1, 1, 10, 100]
-    SUITE["sample (bulk) n=$n k=$k σ=$σ"] = @benchmarkable gaussian_weights_sequential_push($n, σ) rand(Random.default_rng(), _, $k) seconds=1
+    SUITE["sample (bulk) n=$n k=$k σ=$σ"] = @benchmarkable gaussian_weights_sequential_push(n, σ) rand(_, $k)
 end
 
 function pathological1_setup()
