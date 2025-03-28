f46c13376d6bd45b421213cfe2444fc116ba9e51
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-03-28 02:04:32 +0100

    Update invariants.jl

--- test/invariants.jl
+++ test/invariants.jl
@@ -15,7 +15,8 @@ function verify_weights(m::Memory)
 end
 
 function verify_m2(m::Memory)
-    @assert m[2] >= findlast(i -> i == 4 || m[i] != 0, 1:2050)
+    rand(m)
+    @assert m[2] == findlast(i -> i == 4 || m[i] != 0, 1:2050)
 end
 function verify_m4(m::Memory)
     m4 = zero(UInt64)
