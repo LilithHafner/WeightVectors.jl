b2f55893c29f5a6248148801b4d3406d701f959f
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-05-06 17:08:30 +0200

    Update DynamicDiscreteSamplers.jl

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -184,7 +184,7 @@ function _rand(rng::AbstractRNG, m::Memory{UInt64}, n::Integer)
         if m[j] != 0
             q += 1
             inds[q] = j
-            weights[q] = BigInt(get_significand_sum(m, j)) << (j-min_i+1)
+            weights[q] = BigInt(get_significand_sum(m, j)) << (j-min_i)
         end
     end
     counts = multinomial_int(rng, n, weights)
