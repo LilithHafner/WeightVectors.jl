736c8384c8b00b0ce478f02640956f1fb700a8ec
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-05-26 07:44:13 -0500

    Move DynamicDiscreteSampler from src to Test and move tests that depend on it to a separate file
    This commit is strictly moving code, no nonempty lines were created or deleted and no lines were re-ordered

---
+++ test/DynamicDiscreteSampler.jl
@@ -0,0 +1,30 @@
+# Shoe-horn into the legacy DynamicDiscreteSampler API so that we can leverage existing tests
+struct WeightBasedSampler
+    w::ResizableWeights
+end
+WeightBasedSampler() = WeightBasedSampler(ResizableWeights(512))
+
+function Base.push!(wbs::WeightBasedSampler, index, weight)
+    index > length(wbs.w) && resize!(wbs.w, max(index, 2length(wbs.w)))
+    wbs.w[index] = weight
+    wbs
+end
+function Base.append!(wbs::WeightBasedSampler, inds::AbstractVector, weights::AbstractVector)
+    axes(inds) == axes(weights) || throw(DimensionMismatch("inds and weights have different axes"))
+    min_ind,max_ind = extrema(inds)
+    min_ind < 1 && throw(BoundsError(wbs.w, min_ind))
+    max_ind > length(wbs.w) && resize!(wbs.w, max(max_ind, 2length(wbs.w)))
+    for (i,w) in zip(inds, weights)
+        wbs.w[i] = w
+    end
+    wbs
+end
+function Base.delete!(wbs::WeightBasedSampler, index)
+    index ∈ eachindex(wbs.w) && wbs.w[index] != 0 || throw(ArgumentError("Element $index is not present"))
+    wbs.w[index] = 0
+    wbs
+end
+Random.rand(rng::AbstractRNG, st::Random.SamplerTrivial{<:WeightBasedSampler}) = rand(rng, st[].w)
+Random.gentype(::Type{WeightBasedSampler}) = Int
+
+const DynamicDiscreteSampler = WeightBasedSampler
---
+++ test/DynamicDiscreteSampler_tests.jl
@@ -0,0 +1,272 @@
+@testset "basic end to end tests" begin
+    ds = DynamicDiscreteSampler()
+    push!(ds, 1, 1.0)
+    push!(ds, 2, 2.0)
+    push!(ds, 3, 4.0)
+    delete!(ds, 1)
+    delete!(ds, 2)
+    @test rand(ds) == 3
+    push!(ds, 1, 3.0)
+    delete!(ds, 1)
+    @test rand(ds) == 3
+
+    ds = DynamicDiscreteSampler()
+    push!(ds, 1, 5.0)
+    push!(ds, 2, 6.0)
+    delete!(ds, 1)
+    delete!(ds, 2)
+
+    ds = DynamicDiscreteSampler()
+    push!(ds, 1, 1.0)
+    push!(ds, 2, 2.0)
+    delete!(ds, 2)
+
+    ds = DynamicDiscreteSampler()
+    for i in 1:65
+        push!(ds, i, 2.0^i)
+    end
+    delete!(ds, 65)
+    push!(ds, 65, 1.0)
+    delete!(ds, 64)
+end
+
+@testset "randomized end to end tests" begin
+    ds = DynamicDiscreteSampler()
+    x = randperm(100)
+    y = exp.(10*rand(100).-5);
+    push!.((ds,), x, y)
+    for _ in 1:100
+        rand(ds)
+    end
+    for i in randperm(99)
+        delete!(ds, i)
+    end
+    @test rand(ds) == 100
+end
+
+@testset "interleaved randomized end to end tests" begin
+    ds = DynamicDiscreteSampler()
+    elements = Set{Int}()
+    for i in 1:30000
+        if rand() < 0.5
+            i = rand(1:10000)
+            if i ∉ elements
+                push!(ds, i, exp(100randn()))
+                push!(elements, i)
+            end
+        elseif length(elements) > 0
+            element = rand(elements)
+            delete!(ds, element)
+            delete!(elements, element)
+        end
+
+        if length(elements) > 0
+            @test rand(ds) in elements
+        end
+    end
+end
+
+@testset "Targeted statistical tests" begin
+    ds = DynamicDiscreteSampler()
+    for i in 1:3
+        push!(ds, i, float(i))
+    end
+    delete!(ds, 2)
+    @test 0 < count(rand(ds) == 1 for _ in 1:4000) < 1200 # False positivity rate < 4e-13
+end
+
+@testset "Randomized statistical tests" begin
+    rng = StableRNG(42)
+    b = 100
+    range = 1:b
+    weights = [Float64(i) for i in range]
+
+    ds1 = DynamicDiscreteSampler()
+    for (i, w) in zip(range, weights)
+        push!(ds1, i, w)
+    end
+
+    samples_counts = countmap([rand(rng, ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in 1:b]
+    ps_exact = [i/((b ÷ 2)*(b+1)) for i in 1:b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
+
+    samples_counts = countmap([rand(rng, ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in 1:b]
+    ps_exact = [i/((b ÷ 2)*(b+1)) for i in 1:b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
+
+    for i in 1:(b ÷ 2)
+        delete!(ds1, i)
+    end
+
+    samples_counts = countmap([rand(rng, ds1) for _ in 1:10^5])
+    counts_est = [samples_counts[i] for i in (b ÷ 2 + 1):b]
+    ps_exact = [i/((b ÷ 2)*(b+1) - (b ÷ 4)*(b ÷ 2 + 1)) for i in (b ÷ 2 + 1):b]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
+
+    ds2 = DynamicDiscreteSampler()
+
+    append!(ds2, range, weights)
+
+    delete!(ds2, 1)
+    delete!(ds2, 2)
+
+    push!(ds2, 2, 200.0)
+    push!(ds2, 1000, 1000.0)
+
+    samples_counts = countmap(rand(rng, ds2, 10^5))
+    counts_est = [samples_counts[i] for i in [2:b..., 1000]]
+    wsum = (b ÷ 2)*(b+1) - 3 + 200 + 1000
+    ps_exact = [i == 2 ? 200/wsum : i/wsum for i in [2:b..., 1000]]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
+
+    for i in [2:b..., 1000]
+        delete!(ds2, i)
+    end
+
+    push!(ds2, 1, 1.0)
+    push!(ds2, 2, 2.0)
+
+    samples_counts = countmap([rand(rng, ds2) for _ in 1:10^4])
+    counts_est = [samples_counts[1], samples_counts[2]]
+    ps_exact = [1/3, 2/3]
+
+    chisq_test = ChisqTest(counts_est, ps_exact)
+    @test pvalue(chisq_test) > 0.002
+
+    delete!(ds2, 2)
+    @test unique([rand(rng, ds2) for _ in 1:10^3]) == [1]
+end
+
+
+@testset "rng usage tests" begin
+    function getstate_default_rng()
+        t = current_task()
+        (t.rngState0, t.rngState1, t.rngState2, t.rngState3, t.rngState4)
+    end
+    ds = DynamicDiscreteSampler()
+    push!(ds, 1, 1.0)
+    state1 = getstate_default_rng()
+    rand(ds)
+    state2 = getstate_default_rng()
+    @test state1 != state2
+    rand(Xoshiro(42), ds)
+    state3 = getstate_default_rng()
+    @test state2 == state3
+end
+
+@testset "Random API" begin
+    dds = DynamicDiscreteSampler()
+    push!(dds, 1, 1.0)
+    push!(dds, 2, 2.0)
+
+    w = DynamicDiscreteSamplers.FixedSizeWeights(2)
+    w[1] = 1.0
+    w[2] = 2.0
+
+    for source in [dds, w]
+
+        # Conventional usage
+        let x = rand(source)
+            @test x isa Int
+            @test x in 1:2
+        end
+
+        let x = rand(source, 3)
+            @test x isa Vector{Int}
+            @test length(x) == 3
+            @test all(i in 1:2 for i in x)
+        end
+
+        let x = [3, 4, 5]
+            @test rand!(x, source) === x
+            @test all(i in 1:2 for i in x)
+        end
+
+        let x = rand(source, 3, 4, 5)
+            @test x isa Array{Int, 3}
+            @test size(x) == (3, 4, 5)
+            @test all(i in 1:2 for i in x)
+        end
+
+        let x = rand(source, 3, 4, 5)
+            @test x isa Array{Int, 3}
+            @test all(i in 1:2 for i in x)
+        end
+
+        let x = fill(0, 2, 2, 2, 2)
+            @test rand!(x, source) === x
+            @test x isa Array{Int, 4}
+            @test all(i in 1:2 for i in x)
+        end
+
+        # Advanced usage (See https://docs.julialang.org/en/v1/stdlib/Random/#rand-api-hook)
+        for rng in [Random.default_rng(), Xoshiro(42), StableRNG(42), Random.MersenneTwister(42)]
+            let sampler = Random.Sampler(rng, source)
+                x = [rand(rng, sampler) for _ in 1:1000]
+                @test all(xi isa Int for xi in x)
+                @test all(xi in 1:2 for xi in x)
+                @test !allequal(x)
+            end
+            let sampler = Random.Sampler(rng, source, Val(1))
+                x = rand(rng, sampler)
+                @test x isa Int
+                @test x in 1:2
+            end
+            let sampler = Random.Sampler(rng, source, Val(Inf))
+                x = [rand(rng, sampler) for _ in 1:1000]
+                @test all(xi isa Int for xi in x)
+                @test all(xi in 1:2 for xi in x)
+                @test !allequal(x)
+            end
+        end
+    end
+end
+
+@testset "stress test huge probability swings" begin
+    ds = DynamicDiscreteSampler()
+    push!(ds, 1, 1e-300)
+    @test rand(ds) == 1
+    push!(ds, 2, 1e300)
+    @test rand(ds) == 2
+    delete!(ds, 2)
+    @test rand(ds) == 1
+end
+
+function error_d03fb()
+    ds = DynamicDiscreteSampler()
+    for i in 1:1_500
+        push!(ds, i, 0.1)
+    end
+    for i in 1:25_000
+        push!(ds, rand(ds), exp(8randn()))
+    end
+end
+error_d03fb() # This threw AssertionError: 48 <= Base.top_set_bit(m[4]) <= 50 90% of the time on d03fb84d1b62272c5d6ab54c49e643af9b87201b
+
+function error_d03fb_2(n)
+    w = DynamicDiscreteSamplers.FixedSizeWeights(2^n+1);
+    for i in 1:2^n-1
+        w[i] = .99*.5^Base.top_set_bit(i)
+    end
+    w[2^n] = .99
+    w[2^n+1] = 1e100
+    w[2^n+1] = 0
+    @test UInt64(2)^32 < w.m[3]
+end
+error_d03fb_2.(1:15)
+
+ds = DynamicDiscreteSampler()
+push!(ds, 2, 1e308)
+delete!(ds, 2)
+push!(ds, 2, 1e308) # This previously threw
+@test rand(ds) == 2
