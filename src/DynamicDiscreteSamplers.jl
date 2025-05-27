290983053e9c81a5ba51f681fa53f4170610d723
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-05-27 02:30:15 -0500

    Make `Weights` an its subtypes the public API instead of `DynamicDiscreteSampler`/`WeightBasedSampler`. (#129)
    * Move DynamicDiscreteSampler from src to Test and move tests that depend on it to a separate file
    
    This commit is strictly moving code, no nonempty lines were created or deleted and no lines were re-ordered
    
    * Cross-link with include, revise precompile, and split RNG API tests between new and old API
    
    In short, make all the semantic changes associated with the API change and code movement from the previous commit
    
    * benchmarks self compat & load random in benchmerks

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -1,6 +1,7 @@
 module DynamicDiscreteSamplers
 
-export DynamicDiscreteSampler
+VERSION >= v"1.11.0-DEV.469" && eval(Meta.parse("public Weights"))
+export FixedSizeWeights, ResizableWeights, SemiResizableWeights
 
 using Random
 
@@ -793,42 +794,13 @@ end
 # Conform to the AbstractArray API
 Base.size(w::Weights) = (w.m[1],)
 
-# Shoe-horn into the legacy DynamicDiscreteSampler API so that we can leverage existing tests
-struct WeightBasedSampler
-    w::ResizableWeights
-end
-WeightBasedSampler() = WeightBasedSampler(ResizableWeights(512))
-
-function Base.push!(wbs::WeightBasedSampler, index, weight)
-    index > length(wbs.w) && resize!(wbs.w, max(index, 2length(wbs.w)))
-    wbs.w[index] = weight
-    wbs
-end
-function Base.append!(wbs::WeightBasedSampler, inds::AbstractVector, weights::AbstractVector)
-    axes(inds) == axes(weights) || throw(DimensionMismatch("inds and weights have different axes"))
-    min_ind,max_ind = extrema(inds)
-    min_ind < 1 && throw(BoundsError(wbs.w, min_ind))
-    max_ind > length(wbs.w) && resize!(wbs.w, max(max_ind, 2length(wbs.w)))
-    for (i,w) in zip(inds, weights)
-        wbs.w[i] = w
-    end
-    wbs
-end
-function Base.delete!(wbs::WeightBasedSampler, index)
-    index ∈ eachindex(wbs.w) && wbs.w[index] != 0 || throw(ArgumentError("Element $index is not present"))
-    wbs.w[index] = 0
-    wbs
-end
-Random.rand(rng::AbstractRNG, st::Random.SamplerTrivial{<:WeightBasedSampler}) = rand(rng, st[].w)
-Random.gentype(::Type{WeightBasedSampler}) = Int
-
-const DynamicDiscreteSampler = WeightBasedSampler
-
 # Precompile
-precompile(WeightBasedSampler, ())
-precompile(push!, (WeightBasedSampler, Int, Float64))
-precompile(delete!, (WeightBasedSampler, Int))
-precompile(rand, (typeof(Random.default_rng()), WeightBasedSampler))
-precompile(rand, (WeightBasedSampler,))
+precompile(ResizableWeights, (Int,))
+precompile(length, (ResizableWeights,))
+precompile(resize!, (ResizableWeights, Int))
+precompile(setindex!, (ResizableWeights, Float64, Int))
+precompile(getindex, (ResizableWeights, Int))
+precompile(rand, (typeof(Random.default_rng()), ResizableWeights))
+precompile(rand, (ResizableWeights,))
 
 end
