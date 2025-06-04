de885808c2a1d1ce0e94fbd87d5df9e1e1c7079d
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-06-04 13:04:23 +0200

    Update invariants.jl

--- test/invariants.jl
+++ test/invariants.jl
@@ -15,14 +15,14 @@ function verify_weights(m::Memory)
 end
 
 function verify_m2(m::Memory)
-    #@assert m[2] == findlast(i -> i == 4 || m[i] != 0, 1:2051)
+    @assert m[2] == findlast(i -> i == 5 || m[i] != 0, 1:2051)
 end
-function verify_m4(m::Memory)
-    m4 = zero(UInt64)
+function verify_m5(m::Memory)
+    m5 = zero(UInt64)
     for i in 6:2051
-        m4 = Base.checked_add(m4, m[i])
+        m5 = Base.checked_add(m5, m[i])
     end
-    @assert m[4] == m4
+    @assert m[5] == m5
     # @assert m4 == 0 || UInt64(2)^32 <= m4 # This invariant is now maintained loosely and lazily
 end
 
@@ -42,6 +42,6 @@ end
 function verify(m::Memory)
     verify_weights(m)
     verify_m2(m)
-    verify_m4(m)
+    verify_m5(m)
     verify_edit_map_points_to_correct_target(m)
 end
