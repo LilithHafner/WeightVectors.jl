2b44a78c1118ea3e228c927fce425bb8ac00d0ed
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-06-08 00:20:29 +0200

    Update DynamicDiscreteSamplers.jl

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -183,7 +183,7 @@ Random.Sampler(::Type{<:Random.AbstractRNG}, w::Weights, ::Random.Repetition) = 
 Random.gentype(::Type{<:Weights}) = Int
 Base.getindex(w::Weights, i::Int) = _getindex(w.m, i)
 Base.setindex!(w::Weights, v, i::Int) = (_setindex!(w.m, Float64(v), i); w)
-Base.iszero(w::Weights) = m[4] == 0
+Base.iszero(w::Weights) = iszero(w.m[4])
 
 #=@inbounds=# function _rand(rng::AbstractRNG, m::Memory{UInt64})
 
