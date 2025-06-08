d3a4321d561c725ada284cd68458e029f02170c6
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-06-08 03:35:42 +0200

    remove iszero

--- src/DynamicDiscreteSamplers.jl
+++ src/DynamicDiscreteSamplers.jl
@@ -183,7 +183,6 @@ Random.Sampler(::Type{<:Random.AbstractRNG}, w::Weights, ::Random.Repetition) = 
 Random.gentype(::Type{<:Weights}) = Int
 Base.getindex(w::Weights, i::Int) = _getindex(w.m, i)
 Base.setindex!(w::Weights, v, i::Int) = (_setindex!(w.m, Float64(v), i); w)
-Base.iszero(w::Weights) = iszero(w.m[4])
 
 #=@inbounds=# function _rand(rng::AbstractRNG, m::Memory{UInt64})
 
