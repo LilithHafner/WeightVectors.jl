ae1d8ad016f8cd6a3284a2ad3f5788211a4b213d
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-06-11 02:38:05 +0200

    Verify new invariant about number of elements

--- test/invariants.jl
+++ test/invariants.jl
@@ -26,9 +26,10 @@ function verify_m5(m::Memory)
     # @assert m4 == 0 || UInt64(2)^32 <= m4 # This invariant is now maintained loosely and lazily
 end
 
-function verify_edit_map_points_to_correct_target(m::Memory)
+function verify_m4_and_edit_map_points_to_correct_target(m::Memory)
     filled_len = m[1]
     len = _length_from_memory(length(m))
+    m4 = 0
     for i in 1:len
         edit_map_entry = m[i+10524]
         if i > filled_len
@@ -36,12 +37,14 @@ function verify_edit_map_points_to_correct_target(m::Memory)
         elseif edit_map_entry != 0
             @assert m[edit_map_entry>>11 + 1] == i
         end
+        m4 += edit_map_entry != 0
     end
+    @assert m[4] == m4
 end
 
 function verify(m::Memory)
     verify_weights(m)
     verify_m2(m)
     verify_m5(m)
-    verify_edit_map_points_to_correct_target(m)
+    verify_m4_and_edit_map_points_to_correct_target(m)
 end
