4fb15e0dcc710ac6044028d39c933866630b1dae
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2025-07-12 04:11:25 +0200

    support also 32 bit

--- src/WeightVectors.jl
+++ src/WeightVectors.jl
@@ -214,24 +214,25 @@ end
 
 @inline function sample_within_level(rng, m, pos::UInt64, len::UInt64)
     shift = leading_zeros(len-1)
-    # @inbounds safety when Sys.WORD_SIZE == 64: 
+    # @inbounds safety: 
     # 1. 0 <= r <= typemax(UInt64) since r = rand(rng, UInt64)::UInt64
-    # 2. pos ∈ [1, 2^56]. This is explicitly checked and needs to be
-    #    true if m is not corrupted since the maximum length of m is 
-    #    10794+8*2^52 and pos is the absolute position of a group into m.
+    # 2. pos ∈ [1, 2^56]. This is explicitly checked and needs to be true if m is not
+    #    corrupted since the maximum length of m is 10794+8*2^52 and pos is the absolute
+    #    position of a group into m.
     # 3. shift ∈ [8, 64] because shift = leading_zeros(len-1) <= 64 and we check 8 <= shift.
+    #    This also needs to be true if m is not corrupted for the same reason as 2.
     # 4. In the loop, k1 = (r >> shift) and k2 = _convert(Int, k1<<1+pos) so 
-    #    k2+1 = _convert(Int, (r >> shift) << 1 + pos) + 1 where the conversion can be
-    #    spared since (r >> shift) << 1 + pos <= 2^63-1.
-    # 5. max(k2+1) = (typemax(UInt64) >> shift) << 1 + pos + 1 since max((r >> shift) << 1) = 
-    #    (max(r) >> shift) << 1 = (typemax(UInt64) >> shift) << 1 by 1.,2. and 3. 
-    #    Similarly, min(k2+1) = pos + 1 >= 2 by 1.,2. and 3.
+    #    k2+1 = _convert(Int, (r >> shift) << 1 + pos) + 1 <= (r >> shift) << 1 + pos + 1
+    # 5. max(k2+1) <= (typemax(UInt64) >> shift) << 1 + pos + 1 since max((r >> shift) << 1) = 
+    #    (max(r) >> shift) << 1 = (typemax(UInt64) >> shift) << 1 by 1.,2. and 3. and 4.
+    #    Similarly, min(k2+1) = pos + 1 >= 2 by 1.,2. and 3. Note that the inequality of the maximum
+    #    need to be tight if m is not corrupted.
     # 6. Therefore, both m[k2] and m[k2+1] are inbound if we check that 2. holds and if we check
-    #    that max(k2+1) is inbounds. If one of these two conditions doesn't hold, m is corrupted and
-    #    then we throw an error.
+    #    that (typemax(UInt64) >> shift) << 1 + pos + 1 is inbounds. If one of these two conditions
+    #    doesn't hold, m is corrupted and then we throw an error.
     (8 <= shift && UInt64(1) <= pos <= UInt64(2)^56) || throw("Sampler is corrupted")
     checkbounds(m, (typemax(UInt64) >> shift) << 1 + pos + 1)
-    @inbounds_if_word64 while true
+    while true
         r = rand(rng, UInt64)::UInt64
         k1 = (r>>shift)
         k2 = _convert(Int, k1<<1+pos)
