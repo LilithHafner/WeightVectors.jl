d350dceaaec1ae6647c4cad7faa117df359aafce
Adriano Meligrana <68152031+ameligrana@users.noreply.github.com>
2026-04-18 17:38:05 +0200

    Update src/WeightVectors.jl
    Co-authored-by: Lilith Orion Hafner <lilithhafner@gmail.com>

--- src/WeightVectors.jl
+++ src/WeightVectors.jl
@@ -184,7 +184,8 @@ function ensure_sampling_shift!(m::Memory{UInt64}, m5)
     # If the sum of approximate weights becomes less than 2^32, then for performance reasons (to keep the low probability rejection step sufficiently low probability)
     # Increase the shift to a reasonable level.
 	m2 = signed(m[2])
-	m2 == 5 && return
+    # This function is only called by `rand`, so we can assume that the weight vector is non-empty and throw if not.
+    m2 == 5 && throw(ArgumentError("Cannot sample from a WeightVector when all weights are zero"))
 	x = zero(UInt64)
 	offset = 2m2+2093+2
 	checkbounds(m, offset-65*2:offset-2)
